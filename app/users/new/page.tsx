c95a2263d88a44e91e329c31f122d02e738dd8fb
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-10-17 10:46:06 +0530

    Programmatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  const route = useRouter();
+
+  return (
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        route.push("/users");
+      }}
+    >
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
