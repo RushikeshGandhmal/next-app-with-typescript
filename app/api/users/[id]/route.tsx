8a6bdc01a584b34309b2a3d6780ce5e86bd37fbb
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-10-21 23:56:28 +0530

    added REST API endpoints

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,27 @@ export function GET(
 
   return NextResponse.json({ id: 1, name: "RUshi" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "Number is required" }, { status: 400 });
+  }
+  if (params.id > 10) {
+    return NextResponse.json({ error: "User not found." }, { status: 404 });
+  }
+  return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  return NextResponse.json({}, { status: 200 });
+}
