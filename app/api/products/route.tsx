41bc64ccaf7e79a5b76625496fdc68c0509c582f
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-10-31 18:25:25 +0530

    added product

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,15 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "milk", price: 70 },
-    {
-      id: 2,
-      name: "curd",
-      price: 35,
-    },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products, { status: 200 });
 }
 
 export async function POST(request: NextRequest) {
@@ -18,8 +13,10 @@ export async function POST(request: NextRequest) {
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
-  return NextResponse.json(
-    { id: 10, name: body.name, price: body.price },
-    { status: 201 }
-  );
+
+  const newProduct = await prisma.product.create({
+    data: { name: body.name, price: body.price },
+  });
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
