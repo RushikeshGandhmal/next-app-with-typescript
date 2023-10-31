bd370718ed49723d76e3fc5d4da2241fb0711602
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-10-31 15:28:34 +0530

    created prisma client

---
+++ prisma/client.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+// when the second time this file is imported this code will not re-executed, it's cached so the is reused
+// So we use below code from Docs to prevent the issue
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
