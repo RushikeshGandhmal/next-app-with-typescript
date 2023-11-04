1a458ffc57d4bae976ba85cdcfc300c131c524a6
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-11-04 16:53:37 +0530

    protechting routes using Nextjs middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+// import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
+
+// We don't need below code as Nextjs provides us middleware.
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/new-page", request.url));
+// }
+
+// If we want to execute middleware on specific path/api
+export const config = {
+  // *: zero or more
+  // +: one or more
+  // ?: zero or one
+  matcher: ["/dashboard/:path*"],
+};
