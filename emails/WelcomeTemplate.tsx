75ef08f0feea2f88db19461f98942f7596a69ab7
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-11-18 14:20:56 +0530

    styling emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Preview,
@@ -6,18 +6,21 @@ import {
   Container,
   Text,
   Link,
+  Tailwind,
 } from "@react-email/components";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
