bae993898869cf4e9b85ec35ed6bd11a155facc4
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-11-21 14:25:09 +0530

    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,19 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <main>
+    <main className="relative h-screen">
       <h1>Hello {session && <span>{session.user!.name}</span>} </h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="react image"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw (max-width: 786px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
