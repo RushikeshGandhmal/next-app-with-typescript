f8cc8038b74a4df1e44dd964d1a6718202fc18e9
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-11-22 13:37:36 +0530

    SEO

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Metadata } from "next";
+import HeavyComponent from "./components/HeavyComponent";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -10,6 +12,7 @@ export default async function Home() {
   return (
     <main className="relative h-screen">
       <h1>Hello {session && <span>{session.user!.name}</span>} </h1>
+      <HeavyComponent />
       <Link href="/users">Users</Link>
       <ProductCard />
       <Image
@@ -23,4 +26,17 @@ export default async function Home() {
       />
     </main>
   );
+}
+
+// export const metadata: Metadata = {
+//   title: "...", // we're overriding title from root layout metadata
+// };
+
+// When pages have routes/query string parameters we need to generate metadata dynamically
+export async function generateMetadata(): Promise<Metadata> {
+  // const product = await fetch("");
+  // return {
+  //   title: "product.title",
+  // };
+  return {};
 }
