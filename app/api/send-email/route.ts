a1b4073529925bf38344ec95948b5be032183c9d
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-11-24 00:07:55 +0530

    commented code for sending emails

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,8 +1,8 @@
 import WelcomeTemplate from "@/emails/WelcomeTemplate";
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   // await resend.emails.send({
